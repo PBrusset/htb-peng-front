23e99511bafbc8eb4167c574579ceac9ca4d2c54
Ed <ed@jehamilton.co.uk>
2023-03-04 22:52:01 +0000

    Added device data controller placeholder

---
+++ Penguu.App/Controllers/DeviceDataController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Penguu.App.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class DeviceDataController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+
+    public DeviceDataController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+       return Ok(); 
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post()
+    {
+        return Ok();
+    }
+}
