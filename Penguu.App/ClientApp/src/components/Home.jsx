c99628141c6339841735077ce1aa0a2d5f34f73b
PBrusset <47607449+PBrusset@users.noreply.github.com>
2023-03-04 23:01:11 +0000

    front page pics and text

--- Penguu.App/ClientApp/src/components/Home.jsx
+++ Penguu.App/ClientApp/src/components/Home.jsx
@@ -20,12 +20,12 @@ const Home = () => {
                 <div className="container px-5">
                     <div className="row gx-5 align-items-center">
                         <div className="col-lg-6 order-lg-2">
-                            <div className="p-5"><img className="img-fluid rounded-circle" src="01.jpg" alt="..." /></div>
+                            <div className="p-5"><img className="img-fluid rounded-circle" src="drawing-head.svg" alt="..." /></div>
                         </div>
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5">
                                 <h2 className="display-4">Helping you to help those you love</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                                <p>Let us come into your lives to protect your loved ones. Penguu will provide you all the information your need to make sure your caree is doing well. Sit back, relax and relieve your mind knowing the ones you love are safe and sound.</p>
                             </div>
                         </div>
                     </div>
@@ -35,12 +35,12 @@ const Home = () => {
                 <div className="container px-5">
                     <div className="row gx-5 align-items-center">
                         <div className="col-lg-6">
-                            <div className="p-5"><img className="img-fluid rounded-circle" src="02.jpg" alt="..." /></div>
+                            <div className="p-5"><img className="img-fluid rounded-circle" src="drawing-hands.svg" alt="..." /></div>
                         </div>
                         <div className="col-lg-6">
                             <div className="p-5">
                                 <h2 className="display-4">Accessible for everyone</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                                <p> Appliances and devices are made for the majority and often do not cater for unique individuals. Penguu strives to make a more equal world and give back control to its users. Make home feel like home again.</p>
                             </div>
                         </div>
                     </div>
@@ -50,12 +50,12 @@ const Home = () => {
                 <div className="container px-5">
                     <div className="row gx-5 align-items-center">
                         <div className="col-lg-6 order-lg-2">
-                            <div className="p-5"><img className="img-fluid rounded-circle" src="03.jpg" alt="..." /></div>
+                            <div className="p-5"><img className="img-fluid rounded-circle" src="drawing-fam.svg" alt="..." /></div>
                         </div>
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5">
                                 <h2 className="display-4">Designed for you</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                                <p>Human beings are unique and we at Penguu, strive to cater for everyone. Tell us your needs and Penguu will provide devices and create an interface perfectly fitted to you and your loved ones.</p>
                             </div>
                         </div>
                     </div>
